789fdf3598ce3100744d55d15f318840aa7400e8
c2-foonyee <choo2.foonyee@live.uwe.ac.uk>
2022-11-05 15:48:39 +0000

    edited .hpp

--- my_string.hpp
+++ my_string.hpp
@@ -4,24 +4,62 @@
 using namespace std;
 
 class my_string {
+
+private:
     char * str_;
+    int * counter;
+
 public:
 
-    my_string();
-    my_string(const char* str) {
-        str_ = new char[strlen(str) + 1];
-        strcpy(str_, str);
-    }
-
-    my_string(my_string const& s);
-    my_string& operator= (my_string const& s);
-    ~my_string() {
+    my_string(){
         
     }
 
-    char getChar(const int& i) const;
-    void setChar(const int& i, const char& c);
+    my_string(const char* str) {
+        str_ = new char[strlen(str) + 1];
+        strcpy(str_, str);
+        counter = new int;
+        *counter = 1;
+    }
+
+    my_string(my_string const& s) {
+        this-> str_ = s.str_;
+        this-> counter=s.counter;
+        *counter = *counter + 1;
+    }
+
+    my_string& operator= (my_string const& s){
+        if (this == &s)
+        {
+            return *this;
+        }
+
+        this-> str_ = s.str_;
+        this-> counter=s.counter;
+        *counter = *counter + 1;
+        return *this;
+    }
+
+    ~my_string() { // task 3
+        //delete[] str_;
+        *counter = *counter-1;
+
+        if (*counter==0){
+            std::cout << "Refenrence counter ==0 ,hence deleting object :" << str_ << std::endl;
+            delete str_;
+            delete counter;
+        }
+    }
+
+    char getChar(const int& i){
+        return str_[i];
+    }
+    const
+
+    void setChar(const int& i, const char& c){
+        str_[i] = c;
+    }
     void print() {
-        std::cout << str_ << std::endl;
+        std::cout << str_ << " " << "[" <<*counter << "]" << std::endl;
     }
 };
